ac1a206c046b5f91878ebcf3beb00ffd74ff2e2f
yeonhwahong <101hyearth@naver.com>
2023-04-26 00:11:54 +0900

    commit

--- asset/js/common.js
+++ asset/js/common.js
@@ -86,16 +86,16 @@ window.addEventListener('scroll',(e)=>{
 
 //
 const resumeLInk = document.querySelector('.resume_link');
-resumeLInk.addEventListener('click', (e) => {
-    e.preventDefault();
-    const fileURL = '../asset/img/홍연화_이력서_자기소개서.pdf';
-    const fileName = '홍연화_이력서.pdf';
+// resumeLInk.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     const fileURL = '../asset/img/resume.pdf';
+//     const fileName = '홍연화_이력서.pdf';
 
-    const downloadLink = document.createElement("a");
+//     const downloadLink = document.createElement("a");
 
-    downloadLink.setAttribute("href",fileURL);
-    downloadLink.setAttribute("download",fileName);
+//     downloadLink.setAttribute("href",fileURL);
+//     downloadLink.setAttribute("download",fileName);
 
-    downloadLink.click();
-});
+//     downloadLink.click();
+// });
 
